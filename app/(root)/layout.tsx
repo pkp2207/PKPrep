35fd619055c871c3f04be1655d1288c695c3d6cf
Param Kamlesh Pathak <parampathak47@gmail.com>
2025-03-23 11:04:28 +0530

    Home Page UI

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React, { ReactNode } from 'react'
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <div>
+        <div className='root-layout'>
+            <nav>
+                <Link href='/' className='flex items-center gap-2'>
+                    <Image src='/logo.svg' alt='logo' width={38} height={32} />
+                    <h2 className='text-primary-100'>PKPrep</h2>
+                </Link>
+                <Link href='/about'>About</Link>
+            </nav>
             {children}
         </div>
     )
